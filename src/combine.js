428f0c27944839714d37c6b7066de298918e6b9b
Devimal <devimalplanet@gmail.com>
2020-01-24 09:00:29 +0200

    Add edge case handling
    TODO: Implement combine iterative

--- src/combine.js
+++ src/combine.js
@@ -1,32 +1,46 @@
-
-// given provided arrays
-// create all combinations of one item in each array
+// given provided arrays, create all combinations of one item in each array
 function combineRecursive(...arrays) {
-  if (arrays.length > 2) {
-      return combineRecursive(arrays[0], combineRecursive(...arrays.slice(1)));
-  }
+    if (arrays.length < 2) {
+        throw new Error('Cannot combine less than 2 arrays.')
+    }
 
-  const combinations = [];
-  for (const word0 of arrays[0]) {
-      for (const word1 of arrays[1]) {
-          combinations.push(word0 + word1);
-      }
-  }
-  return combinations;
+    if (arrays.length > 2) {
+        return combineRecursive(
+            arrays[0],
+            combineRecursive(...arrays.slice(1))
+        );
+    }
+
+    // arrays.length is exactly 2
+    const combinations = [];
+    for (const word0 of arrays[0]) {
+        for (const word1 of arrays[1]) {
+            combinations.push(word0 + word1);
+        }
+    }
+    return combinations;
 }
 
 function combineIterative(...arrays) {
-  if (arrays.length > 2) {
-      return combineIterative(arrays[0], combineIterative(...arrays.slice(1)));
-  }
+    if (arrays.length < 2) {
+        throw new Error('Cannot combine less than 2 arrays.')
+    }
 
-  const combinations = [];
-  for (const word0 of arrays[0]) {
-      for (const word1 of arrays[1]) {
-          combinations.push(word0 + word1);
-      }
-  }
-  return combinations;
+    if (arrays.length > 2) {
+        return combineRecursive(
+            arrays[0],
+            combineRecursive(...arrays.slice(1))
+        );
+    }
+
+    // arrays.length is exactly 2
+    const combinations = [];
+    for (const word0 of arrays[0]) {
+        for (const word1 of arrays[1]) {
+            combinations.push(word0 + word1);
+        }
+    }
+    return combinations;
 }
 
-module.exports = { combineRecursive, combineIterative }
+module.exports = { combineRecursive, combineIterative };
